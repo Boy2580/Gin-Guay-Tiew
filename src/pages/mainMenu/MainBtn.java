ef2de892e11eb82f90a805f230bd05103a46776e
Boyxam <boycubeu@gmail.com>
2026-02-25 12:06:38 +0700

    Squashed commit of the following:
    commit 3f35b9995b129fd533c1340e3dab2c19513a4e51
    Merge: 69241f8 89b3cb9
    Author: Boyxam <boycubeu@gmail.com>
    Date:   Wed Feb 25 12:05:04 2026 +0700
    
        Merge remote-tracking branch 'origin/levelSelect' into MainMenu-ui
    
    commit 69241f8d04464a3cfdae6ab616f062271554f345
    Author: Boyxam <boycubeu@gmail.com>
    Date:   Wed Feb 25 12:04:29 2026 +0700
    
        update-popup-ui
    
    commit 89b3cb9046a04ee07f28a9404b087c00694bb876
    Author: Beamu <prebeamu@gmail.com>
    Date:   Wed Feb 25 12:03:53 2026 +0700
    
        Fix transition waitTime
    
    commit 79f14a23bae2ece1194d845d2072aaebae410622
    Author: Beamu <prebeamu@gmail.com>
    Date:   Wed Feb 25 11:58:49 2026 +0700
    
        lambda's weird
    
    commit f980efe90d094e72d59a676594fdedf8c0670aa6
    Author: Beamu <prebeamu@gmail.com>
    Date:   Wed Feb 25 11:50:59 2026 +0700
    
        change showPage to toPage
    
    commit f274ddefba7021201ca8b8f77905456e32ac649f
    Author: Beamu <prebeamu@gmail.com>
    Date:   Wed Feb 25 11:43:54 2026 +0700
    
        Created PageNavigation utils

--- src/pages/mainMenu/MainBtn.java
+++ src/pages/mainMenu/MainBtn.java
@@ -4,31 +4,32 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import pages.MainFrame;
+import utilities.PopupWindow;
 
 public class MainBtn implements ActionListener { ;
 
     private JDialog dialog;
     private MainFrame frame;
+    PopupWindow pop = new PopupWindow();
 
-    public MainBtn(){
-        this(null,null);
-    }
-
-    public MainBtn(JDialog dialog, MainFrame frame) {
-        this.dialog = dialog;
+    public MainBtn(MainFrame frame) {
+        if(frame == null) return;
         this.frame = frame;
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Start Game")){
-            frame.showPage("levelSelect");
+            frame.getNavigator().toPage("levelSelect",true, 250);
         }
         if (e.getActionCommand().equals("Tutorial")){
-            dialog.setVisible(true);
+            System.out.println("Tutorial clicked");
+            dialog = pop.createPopup(frame,"tutorial",true, "Do you want to play Tutorial","yes","no");
         }
         if (e.getActionCommand().equals("Shop")){
             System.out.println("Shop");
+            dialog = new PopupWindow().createPopup(frame,"tutorial",true, "Do you want to play Tutorial","no");
         }
         if (e.getActionCommand().equals("Setting")){
             System.out.println("Setting");
---
+++ src/pages/settingMenu/MainSettingPage.java
@@ -0,0 +1,4 @@
+package pages.settingMenu;
+
+public class MainSettingPage {
+}
