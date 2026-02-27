f0825e517cf02464c60d9396d5c8d47622363d78
Username <111523340+BunyaphonCh@users.noreply.github.com>
2026-02-27 18:19:25 +0700

    create upgradeItem in logic

--- src/logic/GameController.java
+++ src/logic/GameController.java
@@ -1,4 +1,7 @@
 package logic;
 
-public class GameController {
+public int getTotalMoney() { return 1000; } // สมมติว่ามีเงิน 1000
+public List<UpgradeItem> getAvailableItems() {
+    return new ArrayList<>(); // คืนค่าลิสต์ว่างไปก่อน จะได้ไม่ Error
 }
+public boolean purchaseItem(UpgradeItem item) { return true; }
